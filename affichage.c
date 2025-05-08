80cdb508e16b23295407ddbae14cdfdb5a15c102
hajardekhissi <hdekhissi07@gmail.com>
2025-05-08 21:31:21 +0200

    .

--- affichage.c
+++ affichage.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
+#include <locale.h>
 
-
-
-void afficherMenu() {
-   
-    printf("============================================\n");
-    printf("        Bienvenue au Refuge ChenYl-Tech      \n");
-    printf("============================================\n");
-    printf("  1. Ajouter un animal\n");
-    printf("  2. Rechercher des animaux\n");
-    printf("  3. Faire adopter un animal\n");
-    printf("  4. Nourrir les animaux\n");
-    printf("  5. Voir l'inventaire\n");
-    printf("  6. Quitter le refuge\n");
-    printf("--------------------------------------------\n");
-    printf("Votre choix : ");
+int main() {
+    setlocale(LC_ALL, "en_US.utf8");
     
+    printf(
+        "╔════════════════════════════════╗\n"
+        "║  Bienvenue au Refuge ChenYl-Tech  ║\n"
+        "╠════════════════════════════════╣\n"
+        "║                                ║\n"
+        "║ 1. Ajouter un animal           ║\n"
+        "║ 2. Rechercher des animaux      ║\n"
+        "║ 3. Faire adopter un animal     ║\n"
+        "║ 4. Nourrir les animaux        ║\n"
+        "║ 5. Voir l'inventaire           ║\n"
+        "║ 6. Quitter                     ║\n"
+        "║                                ║\n"
+        "╚════════════════════════════════╝\n"
+    );
+    
+    return 0;
 }
